cc663fbd93bd38c0a376b390c0b0c97aa42ef7aa
[전시개발팀]최재훈 <jhchoi77@lotte.net>
2023-07-17 13:59:31 +0900

    테스트

--- test/_nuxt/default.08f4448e.js
+++
@@ -1,1 +0,0 @@
-import t from"./header.3e6a082b.js";import{_ as r,r as a,o as s,c as n,b as p,f as c,d}from"./entry.60efb311.js";import"./nuxt-link.25b31116.js";const l={components:{AppHeader:t}};function _(e,f,m,u,i,$){const o=a("AppHeader");return s(),n("div",null,[p(o),c(e.$slots,"default"),d(" layout ")])}const k=r(l,[["render",_]]);export{k as default};
--- test/_nuxt/index.f823e45e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./entry.60efb311.js";const o={};function r(t,s){return n(),c("div",null," index 페이지 ")}const a=e(o,[["render",r]]);export{a as default};
